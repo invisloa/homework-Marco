a836f14c7a92bc8d75f71fccb6022421538d3264
invisloa <91217120+invisloa@users.noreply.github.com>
2022-12-09 22:04:31 +0100

    Add project files.

---
+++ FileOperator.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <string>
+#include <fstream>   
+#include <iostream>
+using namespace std;
+
+class FileOperator
+{
+public:
+	const string readFileName = "d:/input_socket.txt";
+	const string writeFileName = "d:/output_socket.txt";
+	ifstream inFile;        // obiekt obsługujący odczyt z pliku
+	ofstream outFile;
+	string readFileContent;
+
+	void WriteToFile();
+	void ReadFromFile();
+};
+
